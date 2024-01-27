9a4c3cecedacc511fccc8b98752aa0673439d219
thudoan1706 <thudoan1706@gmail.com>
2024-01-26 23:10:49 -0500

    Modify shell script so that it runs user service independently with runme.sh -c

--- a1/runme.sh
+++ a1/runme.sh
@@ -5,4 +5,23 @@
 find . -type f -name "*.sh" -exec chmod +x {} \;
 
 # Run all .sh scripts
-find . -type f -name "*.sh" -execdir sh {} \;
+# find . -type f -name "*.sh" -execdir sh {} \;
+
+# Find the 'compiled' directory within any subdirectory
+compiled_dir=$(find . -type d -name "compiled")
+
+while getopts ":c" opt; do
+  case $opt in
+    c)
+        us_dir="$compiled_dir/UserService"
+
+        if [ -n "$us_dir" ]; then
+            cd "$us_dir"
+            ./runme.sh
+        else
+            echo "Error: 'compiled/UserService' directory not found." >&2
+            exit 1
+        fi
+        ;;
+  esac
+done
