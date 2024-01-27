b773023238ec227468883c76443064816f138d02
thudoan1706 <thudoan1706@gmail.com>
2024-01-26 23:38:27 -0500

    Add arg for shell in user service

--- a1/compiled/UserService/runme.sh
+++ a1/compiled/UserService/runme.sh
@@ -1,24 +1,10 @@
 #!/bin/bash
 # Shell script in a1/compiled/UserService
 
-# Remove all compiled .class files
-find ../../ -name '*.class' -delete
-
 # Set relative paths
 project_root=../../
 us_bin=./bin
 us_lib=../lib
-srcFile=$project_root/src/UserService/src/main/java/
-data=./data/
-
-# Create the bin directory if it doesn't exist
-mkdir -p "$us_bin"
-
-# Create the data directory if it doesn't exist
-mkdir -p "$data"
-
-# Compile Java files
-javac -source 11 -target 11 -d "$us_bin" -cp "$us_lib/jackson-annotations-2.7.9.jar:$us_lib/jackson-core-2.7.9.jar:$us_lib/jackson-databind-2.7.9.jar" "$srcFile"/*.java
 
 # Run Java program
 java -cp "$us_bin:$us_lib/jackson-annotations-2.7.9.jar:$us_lib/jackson-core-2.7.9.jar:$us_lib/jackson-databind-2.7.9.jar" UserServer
--- a1/runme.sh
+++ a1/runme.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
-# Need to document chmod +x runtime.sh scripts
-
 # Make all .sh scripts executable
 find . -type f -name "*.sh" -exec chmod +x {} \;
-
-# Run all .sh scripts
-# find . -type f -name "*.sh" -execdir sh {} \;
 
 # Find the 'compiled' directory within any subdirectory
 compiled_dir=$(find . -type d -name "compiled")
 
-while getopts ":c" opt; do
+
+userSrcFile=./src/UserService/src/main/java/
+
+jar_lib=$compiled_dir/lib/
+
+
+while getopts ":uc" opt; do
   case $opt in
+    u)  echo "Hello"
+        data=$compiled_dir/UserService/data
+
+        # Create the bin directory if it doesn't exist
+        mkdir -p "$us_bin"
+        us_bin=$compiled_dir/UserService/bin
+
+        # Create the data directory if it doesn't exist
+        mkdir -p "$data"
+
+        # Compile User Services
+        javac -source 11 -target 11 -d "$us_bin" -cp "$jar_lib/jackson-annotations-2.7.9.jar:$jar_lib/jackson-core-2.7.9.jar:$jar_lib/jackson-databind-2.7.9.jar" "$userSrcFile"/*.java;;
     c)
         us_dir="$compiled_dir/UserService"
 
